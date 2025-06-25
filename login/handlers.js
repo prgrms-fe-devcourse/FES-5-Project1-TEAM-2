05e2c47749e02a16fd4436e27604e2b3f4663c29
Jaden <hyunbin.kim.3376@gmail.com>
2025-06-25 13:37:36 +0900

    feat: toast 기능 삭제

--- login/handlers.js
+++ login/handlers.js
@@ -1,4 +1,3 @@
-import { showToast } from '../alert/index.js';
 import { apiClient } from './apiClient.js';
 import { API_URL } from './config.js';
 import { elements } from './dom.js';
@@ -8,18 +7,17 @@ import { elements } from './dom.js';
  */
 export const handleLogin = async e => {
   e.preventDefault();
-  // const email = elements.emailInput.value;
-  // const password = elements.passwordInput.value;
+  const email = elements.emailInput.value;
+  const password = elements.passwordInput.value;
 
-  // try {
-  //   const res = await apiClient.post(`${API_URL}/login`, {
-  //     email: email,
-  //     password: password,
-  //   });
+  try {
+    const res = await apiClient.post(`${API_URL}/login`, {
+      email: email,
+      password: password,
+    });
 
-  //   console.log(res);
-  // } catch {}
-  showToast({ message: '야호', type: 'info' });
+    console.log(res);
+  } catch {}
 };
 
 /**
